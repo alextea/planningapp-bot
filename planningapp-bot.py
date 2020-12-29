52618a98e01039fab5aaa2986bb45d1b459a25dd
Alex Torrance <hello@alextorrance.co.uk>
2020-12-29 23:10:01 +0000

    Check if application has already been tweeted

--- planningapp-bot.py
+++ planningapp-bot.py
@@ -1,8 +1,21 @@
 import tweepy
 from datetime import datetime
 import dateutil.parser
+import pickle
 from sodapy import Socrata
 from credentials import *
+
+PICKLEFILE = "planningapp-bot.dat"
+
+try:
+    data = pickle.load(open(PICKLEFILE, "rb"))
+except IOError:
+    data = {
+        "last-modified": ""
+    }
+
+if "tweeted" not in data:
+    data["tweeted"] = []
 
 # Access and authorize our Twitter credentials from credentials.py
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -15,9 +28,16 @@ try:
 except:
     print("Error during authentication")
 
+
+def smart_truncate(content, length=100, suffix='…'):
+    if len(content) <= length:
+        return content
+    else:
+        return ' '.join(content[:length+1].split(' ')[0:-1]) + suffix
+
 # Unauthenticated client only works with public data sets. Note 'None'
 # in place of application token, and no username or password:
-client = Socrata("opendata.camden.gov.uk", None)
+client = Socrata("opendata.camden.gov.uk", app_token)
 
 # Example authenticated client (needed for non-public datasets):
 # client = Socrata(opendata.camden.gov.uk,
@@ -34,13 +54,21 @@ results = client.get(
 )
 
 for result in results:
+    if result['pk'] in data['tweeted']:
+        continue
+
+    date = dateutil.parser.parse(result['registered_date'])
+
+    if date < datetime(2020, 12, 24):
+        data['tweeted'].append(result['pk'])
+        continue
+
     type = result['application_type']
-    if 'applicant_name' in result:
+    if 'applicant_name' in result and result['applicant_name'].strip() != '':
         name = result['applicant_name']
     else:
         name = "Unknown"
-    address = result['development_address']
-    date = dateutil.parser.parse(result['registered_date'])
+    address = smart_truncate(result['development_address'], 36)
     formatted_date = date.strftime("%d %B %Y")
     link = result['full_application']['url']
     if 'location' in result:
@@ -48,7 +76,6 @@ for result in results:
 
     tweet_text = f"New {type} planning application from {name} at {address}. "
     tweet_text += f"Registered on {formatted_date}.\n\n{link}"
-    print(tweet_text, len(tweet_text), "\n\n")
 
     tweet_data = {
         'status': tweet_text,
@@ -57,4 +84,9 @@ for result in results:
         'display_coordinates': True
     }
 
-    # api.update_status(tweet_data)
+    # send tweet
+    api.update_status(tweet_data)
+    data['tweeted'].append(result['pk'])
+    print(f"That's it, that's the tweet: \"{tweet_text}\"", len(tweet_text), "\n\n")
+
+pickle.dump(data, open(PICKLEFILE, "wb"))
